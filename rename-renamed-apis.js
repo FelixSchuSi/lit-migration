4ca7c1d95459b1a03e8b61d62912affa2aad4896
Felix Schulze Sindern <felixs@fh-muenster.de>
2021-04-29 16:56:37 +0200

    Remove js files

--- rename-renamed-apis.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.renameRenamedApis = void 0;
-function renameRenamedApis(_a) {
-    var root = _a.root, j = _a.j;
-    // TODO
-}
-exports.renameRenamedApis = renameRenamedApis;
