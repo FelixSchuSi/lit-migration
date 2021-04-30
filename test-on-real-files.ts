4dc0206d2d46e4802c27e918396d2bb8b76439d8
Felix Schulze Sindern <felixs@fh-muenster.de>
2021-04-30 19:47:25 +0200

    Add real file tests

--- test-on-real-files.ts
+++ test-on-real-files.ts
@@ -2,18 +2,31 @@ import { assert } from 'console';
 import { readdir, readFile } from 'fs/promises';
 
 (async () => {
-    try {
-        const files = await readdir('./__testfixtures__');
-        const testCases = Array.from(new Set(files.map(f => f.split('.')[0])));
-        for (const file of testCases) {
-            console.log(file)
-            const received = await readFile(`./__testfixtures__/${file}.input.ts`);
-            const expected = await readFile(`./__testfixtures__/${file}.output.ts`);
-            assert(received.equals(expected));
-        };
 
+    const passedTests = [];
+    const failedTests = [];
+    const files = await readdir('./__testfixtures__');
+    const testCases = Array.from(new Set(files.map(f => f.split('.')[0])));
+    for (const file of testCases) {
+        const received = await readFile(`./__testfixtures__/${file}.input.ts`);
+        const expected = await readFile(`./__testfixtures__/${file}.output.ts`);
+        if (received.equals(expected)) {
+            passedTests.push(file);
+        } else {
+            failedTests.push(file);
+        }
+    };
 
-    } catch (err) {
-        console.error(err);
+    if (failedTests.length !== 0) {
+        console.error(`
+            ${failedTests.length} tests failed.
+            These tests failed:
+            ${failedTests.join(', ')}
+        `);
+        process.exit(-1);
+    } else {
+        console.log(`${passedTests.length} tests passed ✔`);
     }
+
+
 })()
